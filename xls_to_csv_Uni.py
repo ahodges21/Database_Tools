11c01cdc374d6e7b8b2c7b3d6fb35ac9e612a05b
ahodges21 <ahodges21@student.gsu.edu>
2019-12-11 14:55:38 -0500

    Corrected print statement

--- xls_to_csv_Uni.py
+++ xls_to_csv_Uni.py
@@ -13,7 +13,7 @@ for line in inputfile:
     for j in range(1,len(book.sheet_names())):
         sheet = book.sheet_by_index(j)
         target = path.format(line[-20:-6],"sheet" + str(j) +".csv")
-        print target
+        print(target)
         csvWriter = csv.writer(open(target,"w"), delimiter = '\t')
         for k in range(sheet.nrows):
             csvWriter.writerow(sheet.row_values(k))
